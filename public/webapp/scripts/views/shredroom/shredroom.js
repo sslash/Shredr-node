30bc9212f23e8db5a4aca11658db016cf15599e0
Michael Gunnulfsen <michaelgunnulfsen@Michaels-MacBook-Pro-2.local>
2014-01-21 21:24:39 +0100

    started on backtrack

--- public/webapp/scripts/views/shredroom/shredroom.js
+++ public/webapp/scripts/views/shredroom/shredroom.js
@@ -1,11 +1,18 @@
 define([
 	'backbone',
+
+	'models/shred',
+
 	'views/shredroom/resources',
 	'views/shredroom/tabs',
 	'views/shredroom/upload',
+	'views/shredroom/backtrack',
+
+
 	'hbs!tmpl/shredroom/shredroom_tmpl'
 ],
-function( Backbone, ResourcesView, TabsView, UploadView, ShredroomTmpl  ) {
+function( Backbone, Shred, ResourcesView, TabsView,
+			UploadView, BacktrackView, ShredroomTmpl  ) {
     'use strict';
 
 	/* Return a Layout class definition */
@@ -16,16 +23,17 @@ function( Backbone, ResourcesView, TabsView, UploadView, ShredroomTmpl  ) {
 
 		initialize: function() {
 			console.log("initialize a Shredroom Layout");
+			this.model = new Shred();
 		},
 		
-    	template: ShredroomTmpl,
-    	
+    	template: ShredroomTmpl,    	
 
     	/* Layout sub regions */
     	regions: {
     		resources 	: '#resources-region',
     		tabs 		: '#tabs-region',
-    		upload      : '#upload-region'
+    		upload      : '#upload-region',
+    		backtrack	: '#backtrack-region'
     	},
 
     	/* ui selector cache */
@@ -33,20 +41,33 @@ function( Backbone, ResourcesView, TabsView, UploadView, ShredroomTmpl  ) {
     		resource 	: '#resources-region',
     		tabs 		: '#tabs-region',
     		buttons  	: '#buttons',
-    		upload      : '#upload-region'
+    		upload      : '#upload-region',
+    		backtrack 	: '#backtrack-region'
     	},
 
 		/* Ui events hash */
 		events: {
 			'click #resources' 	: '__resourcesClicked',
 			'click #tabs'		: '__tabsClicked',
-			'click #upload'		: '__uploadClicked'
+			'click #upload'		: '__uploadClicked',
+			'click #jamtracks'	: '__backtrackClicked'
+		},
+
+		__backtrackClicked : function () {
+			if ( !this.BacktrackView ){
+				this.backtrackView = new BacktrackView({model : this.model});
+				this.backtrack.show(this.backtrackView);
+			}
+
+			this.ui.buttons.fadeOut();
+			this.ui.backtrack.show();
 		},
 
 		__uploadClicked : function() {
 			if ( !this.UploadView ) {
-				this.uploadView = new UploadView();
+				this.uploadView = new UploadView({model : this.model});
 				this.upload.show(this.uploadView);
+				this.listenTo(this.uploadView, 'close:event:click', this.uploadModalClosed);
 			}
 
 			this.ui.buttons.fadeOut();
@@ -61,9 +82,8 @@ function( Backbone, ResourcesView, TabsView, UploadView, ShredroomTmpl  ) {
 			}
 
 			this.ui.tabs.show();
-			this.ui.buttons.faceOut();
+			this.ui.buttons.fadeOut();
 			this.ui.tabs.animate({'bottom' : '0'}, 'slow');
-
 		},
 
 		__resourcesClicked : function() {
@@ -85,6 +105,11 @@ function( Backbone, ResourcesView, TabsView, UploadView, ShredroomTmpl  ) {
 
 		resourcesArrowClicked : function () {
 			this.ui.resource.animate({'right' : '-2200px'}, 'slow');
+			this.showButtons();
+		},
+
+		uploadModalClosed : function() {
+			this.ui.upload.fadeOut();
 			this.showButtons();
 		},
 
