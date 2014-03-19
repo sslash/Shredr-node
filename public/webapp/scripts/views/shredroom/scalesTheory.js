06fe70377c933fbcb4686310e0d601c713d95457
Michael Gunnulfsen <michaelgunnulfsen@Michaels-MacBook-Pro-2.local>
2014-03-19 22:46:03 +0100

    started create scales

--- public/webapp/scripts/views/shredroom/scalesTheory.js
+++ public/webapp/scripts/views/shredroom/scalesTheory.js
@@ -2,14 +2,42 @@
 define([
     'backbone',
 
-    'hbs!tmpl/shredroom/scalesTheory'
+    'models/Shred',
+    'views/stage/tabsPreview',
+    'hbs!tmpl/shredroom/scalesTheory',
+    'hbs!tmpl/shredroom/scalesTheoryScaleView',
+    'hbs!tmpl/shredroom/scalesTheoryAddScale'
 ],
-function( Backbone, tmpl ) {
+function( Backbone, Shred, TabsPreview, tmpl, scaleViewTpl, scaleAddTpl ) {
     'use strict';
 
     /* Return a Layout class definition */
     return Backbone.Marionette.ItemView.extend({
 
-        template : tmpl
+        template : tmpl,
+
+        ui : {
+          scaleRegion : '[data-region="scale"]'
+        },
+
+        events : {
+          'click [data-event="add"]' : '__addBtnClicked'
+        },
+
+        onRender : function () {
+            this.ui.scaleRegion.append(scaleViewTpl());
+        },
+
+        __addBtnClicked : function () {
+          this.ui.scaleRegion.fadeOut('fast', function () {
+            var model = new Shred();
+            this.ui.scaleRegion.children().remove();
+            this.ui.scaleRegion.append(scaleAddTpl());
+            this.ui.scaleRegion.fadeIn('fast');
+            this.$('[data-region="tabsArea"]').append(new TabsPreview({
+              model : model
+            }).render().el);
+          }.bind(this));
+        }
     });
 });
